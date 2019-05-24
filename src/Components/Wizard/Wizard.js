c132ba31ff22213a0c14bf0438c550dc0d0e51a4
mnorty <mnorty@gmail.com>
2019-05-24 12:19:53 -0600

    input boxes connected

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -2,12 +2,78 @@ import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Wizard extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: '',
+      address: '',
+      city: '',
+      state: '',
+      zipcode: 0
+    }
+  }
+
+  handleName(name) {
+    this.setState({name:name})
+    console.log(this.state.name)
+  }
+
+  handleAddress(address) {
+    this.setState({address:address})
+    console.log(this.state.address)
+  }
+
+  handleCity(city) {
+    this.setState({city:city})
+    console.log(this.state.city)
+  }
+
+  handleState(state) {
+    this.setState({state:state})
+    console.log(this.state.state)
+  }
+
+  handleZipcode(zipcode) {
+    this.setState({zipcode:zipcode})
+    console.log(this.state.zipcode)
+  }
+
   render(){
     return(
       <div>Wizard
         <Link to ='/'>
           <button>Cancel</button>
         </Link>
+        <input
+         className='inputField'
+         onChange={e => this.handleName(e.target.value)}
+         type = 'text'
+         placeholder='Property Name'
+         />
+         <input
+         className='inputField'
+         onChange={e => this.handleAddress(e.target.value)}
+         type = 'text'
+         placeholder='Property Name'
+         />
+         <input
+         className='inputField'
+         onChange={e => this.handleCity(e.target.value)}
+         type = 'text'
+         placeholder='Property Name'
+         />
+         <input
+         className='inputField'
+         onChange={e => this.handleState(e.target.value)}
+         type = 'text'
+         placeholder='Property Name'
+         />
+         <input
+         className='inputField'
+         onChange={e => this.handleZipcode(e.target.value)}
+         type = 'text'
+         placeholder='Property Name'
+         />
       </div>
     )
   }
