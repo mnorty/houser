aed6463b85fb5380487d091fa6a761c2bc7e1188
mnorty <mnorty@gmail.com>
2019-05-24 12:09:03 -0600

    step 1 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
 import {HashRouter} from 'react-router-dom'
-import Wizard from './Components/Wizard/Wizard'
-import Dashboard from './Components/Dashboard/Dashboard';
 import House from './Components/House/House';
 import Header from './Components/Header/Header';
+import routes from './routes'
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
         <Header/>
-        <Wizard/>
-        <Dashboard/>
+        {routes}
       </div>
     </HashRouter>
   );
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react'
 import House from '../House/House';
+import {Link} from 'react-router-dom'
 
 class Dashboard extends Component {
   render(){
     return(
       <div>Dashboard
         <House/>
+        <Link to='/wizard'>
+          <button>Add new Property</button>
+        </Link>
       </div>
     )
   }
